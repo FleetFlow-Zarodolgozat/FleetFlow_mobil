fb925d8afa72b086bd5ecb529afdc3dc7b0e187b
DomaGamerRDG <ress.dominik.ferenc@students.jedlik.eu>
2026-03-03 10:05:34 +0100

    Add JSON property names and allow cleartext traffic
    Added [JsonPropertyName] attributes to Login and ForgotPassword models for accurate JSON serialization. Enabled cleartext network traffic in AndroidManifest.xml for development and non-HTTPS endpoint support.

--- mobil/mobil/Models/Login.cs
+++ mobil/mobil/Models/Login.cs
@@ -2,20 +2,24 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace mobil.Models
 {
     public class Login
     {
         [Required, EmailAddress]
+        [JsonPropertyName("email")]
         public string? Email { get; set; }
         [Required]
+        [JsonPropertyName("password")]
         public string? Password { get; set; }
     }
 
     public class ForgotPassword
     {
         [Required, EmailAddress]
+        [JsonPropertyName("email")]
         public string? Email { get; set; }
     }
 }
